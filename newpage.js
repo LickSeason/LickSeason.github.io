2ba92d224aa700d21b0b5e17d1028d7ab1ee02c9
LickSeason <35929084+LickSeason@users.noreply.github.com>
2018-02-09 11:43:03 -0500

    Add files via upload

---
+++ newpage.js
@@ -0,0 +1,9 @@
+function myFunction(){
+document.getElementById('first').style.color = 	'#00FFFF';
+document.getElementById('first').style.backgroundColor = 'red';
+document.getElementById('second').style.fontFamily = "Impact"
+window.alert("DISPLAYING SOMETHING!")
+}
+function button(){
+  window.location = 'https://lickseason.github.io/'
+}
